6723b4767297a0f4d16775ff430b4348ae9f2b95
YangkiZhang <yangkizhang@gmail.com>
2017-12-06 16:27:22 +0800

    增加example模块
    Signed-off-by: YangkiZhang <yangkizhang@gmail.com>

---
+++ iooo-boot-example/iooo-boot-example-helloworld/src/main/java/tech/iooo/boot/App.java
@@ -0,0 +1,16 @@
+package tech.iooo.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ * @author Ivan97
+ */
+public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("hello world");
+	}
+}
