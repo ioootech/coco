316941215f642d092e4775e56b3b2fe0e935b6dc
YangkiZhang <yangkizhang@gmail.com>
2018-08-10 17:12:33 +0800

    [REFACTOR]:[add iooo-boot-core dependency]

--- iooo-boot-example/iooo-spring-boot-example/src/main/java/tech/iooo/boot/example/IoooSpringBootExampleApplication.java
+++ iooo-boot-example/iooo-spring-boot-example/src/main/java/tech/iooo/boot/example/IoooSpringBootExampleApplication.java
@@ -1,6 +1,8 @@
 package tech.iooo.boot.example;
 
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,16 +13,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class IoooSpringBootExampleApplication implements CommandLineRunner {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(IoooSpringBootExampleApplication.class);
 	@Autowired
 	private Vertx vertx;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(IoooSpringBootExampleApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		vertx.setPeriodic(1000, id -> logger.info("id:[{}]", id));
 	}
 }
