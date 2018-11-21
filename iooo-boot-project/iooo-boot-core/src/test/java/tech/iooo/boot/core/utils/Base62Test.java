2337a74ca9ca03f9af6db9da08f8e8187325eb16
YangkiZhang <yangkizhang@gmail.com>
2018-11-21 09:51:39 +0800

    update

---
+++ iooo-boot-project/iooo-boot-core/src/test/java/tech/iooo/boot/core/utils/Base62Test.java
@@ -0,0 +1,81 @@
+package tech.iooo.boot.core.utils;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created on 2018/11/21 9:28 AM
+ *
+ * @author <a href="mailto:yangkizhang@gmail.com?subject=iooo-boot">Ivan97</a>
+ */
+@DisplayName("Base62")
+class Base62Test {
+
+  private final Base62 standardEncoder = Base62.createInstance();
+
+  private final Base62[] encoders = {
+      Base62.createInstanceWithGmpCharacterSet(),
+      Base62.createInstanceWithInvertedCharacterSet()
+  };
+
+  @Test
+  @DisplayName("should preserve identity of simple byte arrays")
+  public void preservesIdentity() {
+    for (byte[] message : Environment.getRawInputs()) {
+      for (Base62 encoder : encoders) {
+        final byte[] encoded = encoder.encode(message);
+        final byte[] decoded = encoder.decode(encoded);
+
+        assertArrayEquals(message, decoded);
+      }
+    }
+  }
+
+  @Test
+  @DisplayName("should produce encodings that only contain alphanumeric characters")
+  public void alphaNumericOutput() {
+    for (byte[] message : Environment.getRawInputs()) {
+      for (Base62 encoder : encoders) {
+        final byte[] encoded = encoder.encode(message);
+        final String encodedStr = new String(encoded);
+
+        assertTrue(isAlphaNumeric(encodedStr));
+      }
+    }
+  }
+
+  @Test
+  @DisplayName("should be able to handle empty inputs")
+  public void emptyInputs() {
+    final byte[] empty = new byte[0];
+
+    for (Base62 encoder : encoders) {
+      final byte[] encoded = encoder.encode(empty);
+      assertArrayEquals(empty, encoded);
+
+      final byte[] decoded = encoder.decode(empty);
+      assertArrayEquals(empty, decoded);
+    }
+  }
+
+  @Test
+  @DisplayName("should behave correctly on naive test set")
+  public void naiveTestSet() {
+    for (Map.Entry<String, String> testSetEntry : Environment.getNaiveTestSet().entrySet()) {
+      assertEquals(encode(testSetEntry.getKey()), testSetEntry.getValue());
+    }
+  }
+
+  private String encode(final String input) {
+    return new String(standardEncoder.encode(input.getBytes()));
+  }
+
+  private boolean isAlphaNumeric(final String str) {
+    return str.matches("^[a-zA-Z0-9]+$");
+  }
+}
