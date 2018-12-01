a6b57b1981843f0397653d76f87063006bc69c78
YangkiZhang <yangkizhang@gmail.com>
2018-12-01 13:56:21 +0800

    [FEATURE]:[vertx-guice module]

---
+++ iooo-boot-project/iooo-boot-vertx-guice-module/src/main/java/tech/iooo/boot/guice/vertx/GuiceVertxBinder.java
@@ -0,0 +1,28 @@
+package tech.iooo.boot.guice.vertx;
+
+import com.google.inject.AbstractModule;
+import io.vertx.core.Vertx;
+
+/**
+ * Created on 2018-12-01 13:48
+ *
+ * @author <a href="mailto:yangkizhang@gmail.com?subject=iooo-boot">Ivan97</a>
+ *
+ * Guice {@link AbstractModule} for vertx and container injections
+ */
+public class GuiceVertxBinder extends AbstractModule {
+
+  private final Vertx vertx;
+
+  public GuiceVertxBinder(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void configure() {
+    bind(Vertx.class).toInstance(vertx);
+  }
+}
