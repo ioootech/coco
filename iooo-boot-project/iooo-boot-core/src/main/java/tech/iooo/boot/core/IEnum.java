f14e4b266d024e52d368bddbd8b7778e58999ab2
YangkiZhang <yangkizhang@gmail.com>
2019-03-15 00:13:50 +0800

    update

---
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/IEnum.java
@@ -0,0 +1,19 @@
+package tech.iooo.boot.core;
+
+/**
+ * Created on 2019-03-15 00:06
+ *
+ * @author <a href="mailto:yangkizhang@gmail.com?subject=iooo-boot">Ivan97</a>
+ */
+
+import java.io.Serializable;
+
+public interface IEnum<T extends Serializable> {
+
+  /**
+   * Enum value
+   *
+   * @return T t
+   */
+  T getValue();
+}
