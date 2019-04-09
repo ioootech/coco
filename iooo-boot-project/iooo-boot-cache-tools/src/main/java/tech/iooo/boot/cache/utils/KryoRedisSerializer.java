e7721dfe2ddb74aac59a83b7020c4686c95eac8e
YangkiZhang <yangkizhang@gmail.com>
2019-04-09 10:09:03 +0800

    update

--- iooo-boot-project/iooo-boot-cache-tools/src/main/java/tech/iooo/boot/cache/utils/KryoRedisSerializer.java
+++ iooo-boot-project/iooo-boot-cache-tools/src/main/java/tech/iooo/boot/cache/utils/KryoRedisSerializer.java
@@ -1,8 +1,15 @@
 package tech.iooo.boot.cache.utils;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Registration;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Ivan97
@@ -10,17 +17,69 @@ import com.esotericsoftware.kryo.io.Output;
 @SuppressWarnings("unchecked")
 public class KryoRedisSerializer {
 
-  public static byte[] serialize(Object t) {
+  private static final ThreadLocal<Kryo> kryo = ThreadLocal.withInitial(() -> {
     Kryo kryo = new Kryo();
-    byte[] buffer = new byte[10240000];
-    Output output = new Output(buffer);
-    kryo.writeClassAndObject(output, t);
-    return output.toBytes();
+    // configure kryo instance, customize settings
+    kryo.setReferences(false);
+    kryo.register(Collection.class);
+    kryo.register(Map.class);
+    return kryo;
+  });
+  private static Map<Class, Registration> registrationMap = new ConcurrentHashMap<>();
+
+  public static <T> byte[] serialize(T obj) {
+    Class<T> clazz = (Class<T>) obj.getClass();
+    Kryo kryo = KryoRedisSerializer.kryo.get();
+    if (!registrationMap.containsKey(clazz)) {
+      Registration registration = kryo.register(clazz);
+      registrationMap.put(clazz, registration);
+    }
+    ByteArrayOutputStream outputStream = null;
+    Output output = null;
+    byte[] bytes;
+    try {
+      outputStream = new ByteArrayOutputStream();
+      output = new Output(outputStream);
+      kryo.writeObject(output, obj);
+      output.flush();
+      bytes = outputStream.toByteArray();
+      return bytes;
+    } finally {
+      try {
+        if (output != null) {
+          output.close();
+        }
+        if (outputStream != null) {
+          outputStream.close();
+        }
+      } catch (Exception ignore) {
+
+      }
+    }
   }
 
   public static <T> T deserialize(byte[] bytes) {
-    Kryo kryo = new Kryo();
+    Kryo kryo = KryoRedisSerializer.kryo.get();
     Input input = new Input(bytes);
     return (T) kryo.readClassAndObject(input);
+  }
+
+  public static <T> T deserialize(byte[] data, Class<T> clazz) {
+    Kryo kryo = KryoRedisSerializer.kryo.get();
+    Registration registration = registrationMap.get(clazz);
+    if (registration == null) {
+      registration = kryo.register(clazz);
+      registrationMap.put(clazz, registration);
+    }
+    T object = null;
+    Input input;
+    try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data)) {
+      input = new Input(byteArrayInputStream);
+      object = (T) kryo.readObject(input, registration.getType());
+      input.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return object;
   }
 }
