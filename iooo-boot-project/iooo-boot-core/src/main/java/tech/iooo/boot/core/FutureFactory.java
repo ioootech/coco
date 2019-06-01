64b3b8a6d8efeadbd4d68ca048e036b0ec29b194
龙也 <yanchi.zyc@alibaba-inc.com>
2019-06-01 21:08:04 +0800

    update

--- iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/FutureFactory.java
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/FutureFactory.java
@@ -1,11 +1,17 @@
 package tech.iooo.boot.core;
 
+import tech.iooo.boot.core.utils.ServiceHelper;
+
 /**
  * Created on 2019-03-28 09:17
  *
  * @author <a href="mailto:yangkizhang@gmail.com?subject=iooo-boot">Ivan97</a>
  */
 public interface FutureFactory {
+
+  static FutureFactory instance() {
+    return ServiceHelper.loadFactory(FutureFactory.class);
+  }
 
   <T> Future<T> future();
 
---
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/impl/FutureFactoryImpl.java
@@ -0,0 +1,39 @@
+package tech.iooo.boot.core.impl;
+
+import tech.iooo.boot.core.Future;
+import tech.iooo.boot.core.FutureFactory;
+
+/**
+ * @author ivan97
+ */
+public class FutureFactoryImpl implements FutureFactory {
+
+  private static final SucceededFuture EMPTY = new SucceededFuture<>(null);
+
+  @Override
+  public <T> Future<T> future() {
+    return new FutureImpl<>();
+  }
+
+  @Override
+  public <T> Future<T> succeededFuture() {
+    @SuppressWarnings("unchecked")
+    Future<T> fut = EMPTY;
+    return fut;
+  }
+
+  @Override
+  public <T> Future<T> succeededFuture(T result) {
+    return new SucceededFuture<>(result);
+  }
+
+  @Override
+  public <T> Future<T> failedFuture(Throwable t) {
+    return new FailedFuture<>(t);
+  }
+
+  @Override
+  public <T> Future<T> failureFuture(String failureMessage) {
+    return new FailedFuture<>(failureMessage);
+  }
+}
