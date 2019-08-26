19c832e105e5bf61a5a8d618d3b71186a7bb9b8e
龙也 <yanchi.zyc@alibaba-inc.com>
2019-08-26 09:25:40 +0800

    [REFACTOR]:[update]

--- iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/spring/component/SpringUtils.java
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/spring/component/SpringUtils.java
@@ -1,11 +1,13 @@
 package tech.iooo.boot.core.spring.component;
 
+import java.lang.annotation.Annotation;
+import java.util.Map;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import tech.iooo.boot.core.constants.SuppressTypeConstants;
 
 /**
  * @author yanchi.zyc
@@ -14,19 +16,30 @@ import tech.iooo.boot.core.constants.SuppressTypeConstants;
 @Component
 public class SpringUtils implements ApplicationContextAware {
 
-  private static ApplicationContext APPLICATION_CONTEXT = null;
+  private static ApplicationContext APPLICATION_CONTEXT;
 
   @Override
   public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
     APPLICATION_CONTEXT = applicationContext;
   }
 
-  @SuppressWarnings(SuppressTypeConstants.UNCHECKED)
-  public static <T> T getBean(String name) {
-    return (T) APPLICATION_CONTEXT.getBean(name);
-  }
-
   public static <T> T getBean(Class<T> clazz) {
     return APPLICATION_CONTEXT.getBean(clazz);
+  }
+
+  public static Object getBean(String clazz) {
+    return APPLICATION_CONTEXT.getBean(clazz);
+  }
+
+  public static <T> Map<String, T> getBeansOfType(Class<T> tClass) {
+    return APPLICATION_CONTEXT.getBeansOfType(tClass);
+  }
+
+  public static Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> tClass) {
+    return APPLICATION_CONTEXT.getBeansWithAnnotation(tClass);
+  }
+
+  public static Environment getEnvironment() {
+    return APPLICATION_CONTEXT.getEnvironment();
   }
 }
