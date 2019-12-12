12881a875f0a2c95cbdc3eb195a221c8c7885f54
龙也 <yanchi.zyc@alibaba-inc.com>
2019-12-12 10:55:02 +0800

    [REFACTOR]:[update]

--- iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/function/Operator.java
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/function/Operator.java
@@ -9,19 +9,21 @@ import java.util.function.UnaryOperator;
  */
 public interface Operator<T> extends UnaryOperator<T> {
 
+
   /**
-   * 操作对象
+   * Applies this operator to the given argument.
    *
-   * @param t input
-   * @return output
+   * @param t the operator argument
+   * @return the operator result;
    */
   T operate(T t);
 
+
   /**
-   * 操作对象
+   * Applies this operator to the given argument.
    *
-   * @param t input
-   * @return output
+   * @param t the operator argument
+   * @return the operator result;
    */
   @Override
   default T apply(T t) {
@@ -29,13 +31,42 @@ public interface Operator<T> extends UnaryOperator<T> {
   }
 
   /**
-   * 后续操作
+   * Returns a composed operator that first applies the {@code before} operator to its input, and then applies this operator to the result. If evaluation of either operator throws an exception, it is relayed to the caller of the composed
+   * operator.
    *
-   * @param after after
-   * @return Operator
+   * @param before the operator to apply before this operator is applied
+   * @return a composed operator that first applies the {@code before} operator and then applies this operator
+   * @throws NullPointerException if before is null;
+   * @see #andThen(Operator)
+   */
+  default Operator<T> compose(Operator<T> before) {
+    Objects.requireNonNull(before);
+    return t -> operate(before.operate(t));
+  }
+
+
+  /**
+   * Returns a composed operator that first applies this operator to its input, and then applies the {@code after} operator to the result. If evaluation of either operator throws an exception, it is relayed to the caller of the composed
+   * operator.
+   *
+   * @param after the operator to apply after this operator is applied
+   * @return a composed operator that first applies this operator and then applies the {@code after} operator
+   * @throws NullPointerException if after is null;
+   * @see #compose(Operator)
    */
   default Operator<T> andThen(Operator<T> after) {
     Objects.requireNonNull(after);
     return t -> after.operate(operate(t));
+  }
+
+
+  /**
+   * Returns an operator that always returns its input argument.
+   *
+   * @param <T> the type of the input and output objects to the function
+   * @return an operator that always returns its input argument;
+   */
+  static <T> Operator<T> identity() {
+    return t -> t;
   }
 }
