f66642088ea20dc7d7af8fdfdb2c053b56055223
龙也 <yanchi.zyc@alibaba-inc.com>
2020-01-15 14:06:00 +0800

    [REFACTOR]:[update]

--- iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/Promise.java
+++ iooo-boot-project/iooo-boot-core/src/main/java/tech/iooo/boot/core/Promise.java
@@ -24,7 +24,13 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    * @param asyncResult the async result to handle
    */
   @Override
-  void handle(AsyncResult<T> asyncResult);
+  default void handle(AsyncResult<T> asyncResult) {
+    if (asyncResult.succeeded()) {
+      complete(asyncResult.result());
+    } else {
+      fail(asyncResult.cause());
+    }
+  }
 
   /**
    * Set the result. Any handler will be called, if there is one, and the promise will be marked as completed.
@@ -34,14 +40,22 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    * @param result the result
    * @throws IllegalStateException when the promise is already completed
    */
-  void complete(T result);
+  default void complete(T result) {
+    if (!tryComplete(result)) {
+      throw new IllegalStateException("Result is already complete");
+    }
+  }
 
   /**
    * Calls {@code complete(null)}
    *
    * @throws IllegalStateException when the promise is already completed
    */
-  void complete();
+  default void complete() {
+    if (!tryComplete()) {
+      throw new IllegalStateException("Result is already complete");
+    }
+  }
 
   /**
    * Set the failure. Any handler will be called, if there is one, and the future will be marked as completed.
@@ -49,7 +63,11 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    * @param cause the failure cause
    * @throws IllegalStateException when the promise is already completed
    */
-  void fail(Throwable cause);
+  default void fail(Throwable cause) {
+    if (!tryFail(cause)) {
+      throw new IllegalStateException("Result is already complete");
+    }
+  }
 
   /**
    * Calls {@link #fail(Throwable)} with the {@code message}.
@@ -57,7 +75,11 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    * @param message the failure message
    * @throws IllegalStateException when the promise is already completed
    */
-  void fail(String message);
+  default void fail(String message) {
+    if (!tryFail(message)) {
+      throw new IllegalStateException("Result is already complete");
+    }
+  }
 
   /**
    * Like {@link #complete(Object)} but returns {@code false} when the promise is already completed instead of throwing an {@link IllegalStateException}, it returns {@code true} otherwise.
@@ -72,7 +94,9 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    *
    * @return {@code false} when the future is already completed
    */
-  boolean tryComplete();
+  default boolean tryComplete() {
+    return tryComplete(null);
+  }
 
   /**
    * Like {@link #fail(Throwable)} but returns {@code false} when the promise is already completed instead of throwing an {@link IllegalStateException}, it returns {@code true} otherwise.
@@ -88,11 +112,12 @@ public interface Promise<T> extends Handler<AsyncResult<T>> {
    * @param message the failure message
    * @return false when the future is already completed
    */
-  boolean tryFail(String message);
+  default boolean tryFail(String message) {
+    return tryFail(new IllegalStateException(message));
+  }
 
   /**
    * @return the {@link Future} associated with this promise, it can be used to be aware of the promise completion
    */
   Future<T> future();
 }
-
